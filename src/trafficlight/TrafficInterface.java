e975df69756b9c2463eb9c89695494ac713bad20
Mike Crawford <mikec@unitedtitanium.com>
2019-08-21 13:34:26 -0400

    Initial Commit

---
+++ src/trafficlight/TrafficInterface.java
@@ -0,0 +1,17 @@
+
+package trafficlight;
+
+/** @author mikec */
+public interface TrafficInterface 
+{
+    public static final int RED = 0;
+    public static final int GREEN = 1;
+    public static final int YELLOW = 2;
+    
+    public static final int RED_ON = 0;
+    public static final int RED_OFF = 1;
+    public static final int GREEN_ON = 2;
+    public static final int GREEN_OFF = 3;
+    public static final int YELLOW_ON = 4;
+    public static final int YELLOW_OFF = 5;
+}
